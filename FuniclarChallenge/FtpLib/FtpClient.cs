c20606f0a5115374afa2b3f968b2f7c996410859
Alexander Wiedemann <wiedemann@bluehands.de>
2021-05-07 14:16:25 +0200

    - add funicular challenge

---
+++ FuniclarChallenge/FtpLib/FtpClient.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FtpLib
+{
+    public class FtpClient
+    {
+        public Task<FtpConnection> Connect(Uri host, int port, FtpCredentials credentials) => Task.FromResult(new FtpConnection(true, null));
+
+        public Task UploadFile(FtpConnection connection, byte[] fileToUpload, string targetFolder) => Task.CompletedTask;
+    }
+
+    public class FtpConnection : IDisposable
+    {
+        public bool IsConnected { get; }
+        public string? ConnectError { get; }
+
+        internal FtpConnection(bool isConnected, string? connectError)
+        {
+            IsConnected = isConnected;
+            ConnectError = connectError;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+
+    public abstract class FtpCredentials
+    {
+        public static FtpCredentials Password(string username, string password) => new Password_(username, password);
+        public static FtpCredentials PrivateKey(string username, byte[] privateKey) => new PrivateKey_(username, privateKey);
+
+        public string Username { get; }
+
+        public class Password_ : FtpCredentials
+        {
+            public string Password { get; }
+
+            public Password_(string username, string password) : base(UnionCases.Password, username) => Password = password;
+        }
+
+        public class PrivateKey_ : FtpCredentials
+        {
+            public byte[] PrivateKey { get; }
+
+            public PrivateKey_(string username, byte[] privateKey) : base(UnionCases.PrivateKey, username) => PrivateKey = privateKey;
+        }
+
+        internal enum UnionCases
+        {
+            Password,
+            PrivateKey
+        }
+
+        internal UnionCases UnionCase { get; }
+        FtpCredentials(UnionCases unionCase, string username)
+        {
+            UnionCase = unionCase;
+            Username = username;
+        }
+
+        public override string ToString() => Enum.GetName(typeof(UnionCases), UnionCase) ?? UnionCase.ToString();
+        bool Equals(FtpCredentials other) => UnionCase == other.UnionCase;
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((FtpCredentials)obj);
+        }
+
+        public override int GetHashCode() => (int)UnionCase;
+    }
+
+    public static class FtpCredentialsExtension
+    {
+        public static T Match<T>(this FtpCredentials ftpCredentials, Func<FtpCredentials.Password_, T> password, Func<FtpCredentials.PrivateKey_, T> privateKey)
+        {
+            switch (ftpCredentials.UnionCase)
+            {
+                case FtpCredentials.UnionCases.Password:
+                    return password((FtpCredentials.Password_)ftpCredentials);
+                case FtpCredentials.UnionCases.PrivateKey:
+                    return privateKey((FtpCredentials.PrivateKey_)ftpCredentials);
+                default:
+                    throw new ArgumentException($"Unknown type derived from FtpCredentials: {ftpCredentials.GetType().Name}");
+            }
+        }
+
+        public static async Task<T> Match<T>(this FtpCredentials ftpCredentials, Func<FtpCredentials.Password_, Task<T>> password, Func<FtpCredentials.PrivateKey_, Task<T>> privateKey)
+        {
+            switch (ftpCredentials.UnionCase)
+            {
+                case FtpCredentials.UnionCases.Password:
+                    return await password((FtpCredentials.Password_)ftpCredentials).ConfigureAwait(false);
+                case FtpCredentials.UnionCases.PrivateKey:
+                    return await privateKey((FtpCredentials.PrivateKey_)ftpCredentials).ConfigureAwait(false);
+                default:
+                    throw new ArgumentException($"Unknown type derived from FtpCredentials: {ftpCredentials.GetType().Name}");
+            }
+        }
+
+        public static async Task<T> Match<T>(this Task<FtpCredentials> ftpCredentials, Func<FtpCredentials.Password_, T> password, Func<FtpCredentials.PrivateKey_, T> privateKey) => (await ftpCredentials.ConfigureAwait(false)).Match(password, privateKey);
+        public static async Task<T> Match<T>(this Task<FtpCredentials> ftpCredentials, Func<FtpCredentials.Password_, Task<T>> password, Func<FtpCredentials.PrivateKey_, Task<T>> privateKey) => await(await ftpCredentials.ConfigureAwait(false)).Match(password, privateKey).ConfigureAwait(false);
+    }
+}
---
+++ FuniclarChallenge/README.md
@@ -0,0 +1,9 @@
+# Funicular-Switch challenge
+
+Welcome!
+Try to refactor the SendFileToFtp program, without changing parameters of Main (FtpLib is an 'external' library and cannot be modified). 
+Make main return 0 in success and 1 in error case and print useful error information to console in case of invalid inputs or ftp errors.
+Use the `Result` type from Funicular.Switch nuget package, introduction can be found on github (https://github.com/bluehands/Funicular-Switch).
+Useful helper methods are `Map`, `Bind`, `Match` to build the pipeline, `Validate` to perform checks on input and `Try` to turn exceptions into Result(s).
+
+Goal is to collect as much error information as possible and to respect *'Single Level of Abstraction'* and *'Do not Repeat Yourself'* principles.
