7c5f4f3da563b00267378505ac50b748e7bf66ce
Alexander Wiedemann <wiedemann@bluehands.de>
2024-11-19 13:19:43 +0100

    - beginner sample

---
+++ ResultTypesWorkshop/Beginner/WithExceptions.cs
@@ -0,0 +1,48 @@
+﻿using Beginner.World;
+using FluentAssertions;
+
+namespace Beginner;
+
+using ExceptionCastle;
+
+[TestClass]
+public sealed class WithExceptions
+{
+    [TestMethod]
+    public void ShaveThePrince()
+    {
+        var prince = Castle.FindPrince();
+        AwakePrince? awakePrince = null;
+        if (prince is SleepingPrince sleepingPrince)
+        {
+            try
+            {
+                awakePrince = sleepingPrince.Awake();
+            }
+            catch (Exception e)
+            {
+                Insta.Post($"You won't believe it.... : {e.Message}");
+            }
+        }
+        else
+            awakePrince = (AwakePrince)prince;
+
+        if (awakePrince != null)
+        {
+            var razor = Castle.FindRazor();
+            if (razor != null)
+            {
+                var beautifulPrince = awakePrince.Shave(razor);
+                var picture = beautifulPrince.TakePicture();
+                Insta.Post(picture);
+            }
+            else
+            {
+                Insta.Post("You won't believe it....: Razor missing in castle!!!");
+            }
+        }
+
+
+        Insta.Posts.Should().BeGreaterThan(0);
+    }
+}
---
+++ ResultTypesWorkshop/Beginner/World/Insta.cs
@@ -0,0 +1,21 @@
+﻿namespace Beginner.World
+{
+    static class Insta
+    {
+        public static void Post(Picture picture)
+        {
+            Console.WriteLine("Cool picture!!!");
+            Posts++;
+        }
+
+        public static void Post(string text)
+        {
+            Console.WriteLine(text);
+            Posts++;
+        }
+
+        public static int Posts { get; private set; }
+    }
+
+    record Picture;
+}
