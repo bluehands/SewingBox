b1dc2cda51b791ac9bbe2cf74ee51c8d84308a9c
David Retzlaff <david.retzlaff@bluehands.de>
2026-03-26 14:54:19 +0100

    feat(interceptors): add performance logging for all ITodoRepository methods and update .gitignore

--- Interceptors/Interceptors/TodoInterceptors.cs
+++ Interceptors/Interceptors/TodoInterceptors.cs
@@ -1,4 +1,6 @@
 using System.Diagnostics;
+using FunicularSwitch;
+using FunicularSwitch.Generic;
 using Splice;
 
 namespace Interceptors;
@@ -12,6 +14,56 @@ public static partial class TodoInterceptors
         var begin = Stopwatch.GetTimestamp();
         var writer = await repo.GetAll();
         var duration = Stopwatch.GetElapsedTime(begin);
-        return writer.Write($"{nameof(ITodoRepository.GetAll)} todos took {duration.TotalMilliseconds} ms");
+        var (result, elements) = writer;
+        return Writer.Write(result, elements.Add($"{nameof(ITodoRepository.GetAll)} todos took {duration.TotalMilliseconds} ms"));
+    }
+
+    [Interceptor<ITodoRepository>(nameof(ITodoRepository.GetById))]
+    public static partial async Task<Writer<GenericResult<Todo, Error>, string>> GetById(
+        this ITodoRepository repo,
+        Guid id)
+    {
+        var begin = Stopwatch.GetTimestamp();
+        var writer = await repo.GetById(id);
+        var duration = Stopwatch.GetElapsedTime(begin);
+        var (result, elements) = writer;
+        return Writer.Write(result, elements.Add($"{nameof(ITodoRepository.GetById)} {id} took {duration.TotalMilliseconds} ms"));
+    }
+
+    [Interceptor<ITodoRepository>(nameof(ITodoRepository.Add))]
+    public static partial async Task<Writer<GenericResult<Todo, Error>, string>> Add(
+        this ITodoRepository repo,
+        CreateTodoDto dto)
+    {
+        var begin = Stopwatch.GetTimestamp();
+        var writer = await repo.Add(dto);
+        var duration = Stopwatch.GetElapsedTime(begin);
+        var (result, elements) = writer;
+        return Writer.Write(result, elements.Add($"{nameof(ITodoRepository.Add)} todo '{dto.Title}' took {duration.TotalMilliseconds} ms"));
+    }
+
+    [Interceptor<ITodoRepository>(nameof(ITodoRepository.Update))]
+    public static partial async Task<Writer<GenericResult<Todo, Error>, string>> Update(
+        this ITodoRepository repo,
+        Guid id,
+        UpdateTodoDto dto)
+    {
+        var begin = Stopwatch.GetTimestamp();
+        var writer = await repo.Update(id, dto);
+        var duration = Stopwatch.GetElapsedTime(begin);
+        var (result, elements) = writer;
+        return Writer.Write(result, elements.Add($"{nameof(ITodoRepository.Update)} {id} took {duration.TotalMilliseconds} ms"));
+    }
+
+    [Interceptor<ITodoRepository>(nameof(ITodoRepository.Delete))]
+    public static partial async Task<Writer<GenericResult<FunicularSwitch.Unit, Error>, string>> Delete(
+        this ITodoRepository repo,
+        Guid id)
+    {
+        var begin = Stopwatch.GetTimestamp();
+        var writer = await repo.Delete(id);
+        var duration = Stopwatch.GetElapsedTime(begin);
+        var (result, elements) = writer;
+        return Writer.Write(result, elements.Add($"{nameof(ITodoRepository.Delete)} {id} took {duration.TotalMilliseconds} ms"));
     }
 }
